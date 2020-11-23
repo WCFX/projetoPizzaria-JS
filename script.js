965cac5d9e8695ee4fd30abe135e4d0ff541fbcf
wcfx <wcfx.dev@gmail.com>
2020-11-23 17:23:25 -0300

    Setando o numero padrao 1 para as pizzas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const modalQuantidade = 1;
 const query = (el) => document.querySelector(el);
 const queryAll = (el) => document.querySelectorAll(el);
 
@@ -13,16 +14,22 @@ pizzaJson.map((pizza, index) => {
   pizzaItem.querySelector('a').addEventListener('click', (e) => {
     e.preventDefault()
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    modalQuantidade = 1;
 
     query('.pizzaInfo h1').innerHTML = pizzaJson[key].name
     query('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
     query('.pizzaBig img').src = pizzaJson[key].img
     query('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+    query('.pizzaInfo--size.selected').classList.remove('selected');
     queryAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+      if(sizeIndex == 2){
+        size.classList.add('selected');
+      }
+
       size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
     })
 
-    
+    query('.pizzaInfo--qt').innerHTML = modalQuantidade;
     query('.pizzaWindowArea').style.opacity=0;
     query('.pizzaWindowArea').style.display='flex';
     setTimeout(() => {
