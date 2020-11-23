c47fd4bc5cd77858ab29ca50974613cefa45e831
wcfx <wcfx.dev@gmail.com>
2020-11-23 17:30:54 -0300

    Adicionar funcoes do modal

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const modalQuantidade = 1;
+let modalQuantidade = 1;
 const query = (el) => document.querySelector(el);
 const queryAll = (el) => document.querySelectorAll(el);
 
@@ -16,20 +16,20 @@ pizzaJson.map((pizza, index) => {
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
     modalQuantidade = 1;
 
+    query('.pizzaBig img').src = pizzaJson[key].img
     query('.pizzaInfo h1').innerHTML = pizzaJson[key].name
     query('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
-    query('.pizzaBig img').src = pizzaJson[key].img
     query('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
     query('.pizzaInfo--size.selected').classList.remove('selected');
     queryAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
       if(sizeIndex == 2){
         size.classList.add('selected');
       }
-
       size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
-    })
+    });
 
     query('.pizzaInfo--qt').innerHTML = modalQuantidade;
+
     query('.pizzaWindowArea').style.opacity=0;
     query('.pizzaWindowArea').style.display='flex';
     setTimeout(() => {
@@ -39,4 +39,7 @@ pizzaJson.map((pizza, index) => {
   });
 
   query('.pizza-area').append(pizzaItem);
-})
+});
+
+
+
